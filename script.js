70988b08733c1372915d87b9aa6c98d9833e7869
JotaJotaM1 <johnjames_89@hotmail.com>
2024-02-21 19:07:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Header Nav */
+
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+
+/* Google Analitycs */
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-329J6BK6HG");
